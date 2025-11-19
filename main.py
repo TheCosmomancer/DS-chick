bf00f983dca1fc2e0976ea81dbae1fa464e4d2bc
Cosmomancer <cosmomancer@proton.me>
2025-11-19 13:57:44 +0330

    .

--- main.py
+++ main.py
@@ -1,113 +1,197 @@
 class UsolvedRequirementException(Exception):
     pass
+
+
 class Heap:
-    def __init__(self):
-        self.array = []
-    def findAnswerByID(self,id):
-        for i in range(len(self.array)):
-            if self.array[i].id == id:
-                if self.array[i].answer is None:
-                    raise UsolvedRequirementException(
-                        "Expression requested another expression that is not solved yet"
-                    )
-                return self.array[i].answer
-    def addItem(self,item):
+    def __init__(self, array=None):
+        self.array = array if array is not None else []
+
+    def addItem(self, item):
         self.array.append(item)
         i = len(self.array) - 1
-        while i > 0 and self.array[i].priority < self.array[(i-1)//2].priority:
-            self.array[i], self.array[(i-1)//2] = self.array[(i-1)//2], self.array[i]
-            i = (i-1)//2
+        while i > 0 and self.array[i].priority < self.array[(i - 1) // 2].priority:
+            self.array[i], self.array[(i - 1) // 2] = (
+                self.array[(i - 1) // 2],
+                self.array[i],
+            )
+            i = (i - 1) // 2
+    def extractMin(self):
+        if len(self.array) == 0:
+            return None
+        
+        # Store min element (root)
+        minItem = self.array[0]
+        
+        # Move last element to root
+        self.array[0] = self.array[-1]
+        self.array.pop()
+        
+        # Heapify down
+        i = 0
+        while True:
+            smallest = i
+            left = 2 * i + 1
+            right = 2 * i + 2
+            
+            if left < len(self.array) and self.array[left].priority < self.array[smallest].priority:
+                smallest = left
+            if right < len(self.array) and self.array[right].priority < self.array[smallest].priority:
+                smallest = right
+            
+            if smallest == i:
+                break
+                
+            self.array[i], self.array[smallest] = self.array[smallest], self.array[i]
+            i = smallest
+        
+        return minItem
     def solve(self):
-        lastunsolved = {}
-        unsolved = {self.array[x].id:False for x in range(len(self.array))}
-        while False in unsolved.values():
-            for i in range(len(self.array)):
-                if self.array[i].answer is None:
-                    try:
-                        self.array[i].answer = Expression.solve(self.array[i].expression,self)[0]
-                        unsolved[self.array[i].id] = True
-                    except:
-                        pass
-            if unsolved == lastunsolved:
+        solved = Heap()
+        last_solved_count = -1
+        
+        while len(self.array) > 0:
+            # Check if we made progress in the last iteration
+            if len(solved.array) == last_solved_count:
                 return "Cycle detected"
-            lastunsolved = unsolved.copy()
+            last_solved_count = len(solved.array)
+            
+            # Try to solve all remaining expressions
+            unsolved_heap = Heap()
+            while tosolve := self.extractMin():
+                try:
+                    tosolve.answer = Expression.solve(tosolve.expression, Heap(solved.array.copy()))[0]
+                    solved.addItem(tosolve)
+                except UsolvedRequirementException:
+                    unsolved_heap.addItem(tosolve)
+            
+            # Put unsolved items back for next iteration
+            self.array = unsolved_heap.array
+
         ret = ""
-        for i in range(len(self.array)):
-            ret += str(self.array[i].id) + ": " + str(self.array[i].answer) + "\n"
+        while solveditem := solved.extractMin():
+            ret += str(solveditem.id) + ": " + str(solveditem.answer) + "\n"
         return ret
 
+
+class Stack:
+    def __init__(self):
+        self.array = []
+
+    def push(self, item):
+        self.array.append(item)
+
+    def pop(self):
+        ret = self.array[-1]
+        self.array.pop()
+        return ret
+    def isEmpty(self):
+        return len(self.array) == 0
 class Expression:
-    def __init__(self,id,priority,expression,answer = None):
+    def __init__(self, id, priority, expression, answer=None):
         self.id = id
         self.priority = priority
         self.expression = expression
         self.answer = answer
+
     @staticmethod
     def solve(expression, heap, i=0):
-        expStack = []
-        operate = False
-        while expression[i]!=')':
-            if not operate:
-                if expression[i]=='(':
-                    answer, i = Expression.solve(expression, i + 1)
-                    expStack.append(answer)
-                elif expression[i].isdigit():
-                    expStack.append(int(expression[i]))
+        outputStack = Stack()
+        operatorStack = Stack()
+        precedence = {"'": 4, "^": 3, "*": 2, "/": 2, "%": 2, "+": 1, "-": 1}
+        while expression[i] != ")":
+            # print(f'{expression}, {i}: {expression[i]}\n')
+            if expression[i] == "(":
+                # print("calling solve")
+                answer, i = Expression.solve(expression, heap, i + 1)
+                outputStack.push(answer)
+            elif expression[i].isdigit():
+                outputStack.push(int(expression[i]))
+                i += 1
+            elif expression[i] == "[":
+                temp = ""
+                i += 1
+                while expression[i] != "]":
+                    temp += expression[i]
                     i += 1
-                elif expression[i]=='[':
-                    temp = ''
-                    i += 1
-                    while expression[i]!=']':
-                        temp += expression[i]
-                        i += 1
-                    i += 1
-                    temp = int(temp)
-                    answer = heap.findAnswerByID(temp)#TODO
-                    expStack.append(answer)
-                else:
-                    expStack.append(expression[i])
-                    operate = True
-                    i += 1
+                i += 1
+                answer = None
+                while potentialAnswer := heap.extractMin():
+                    if potentialAnswer.id == int(temp):
+                        answer = potentialAnswer.answer
+                        break
+                    else:
+                        heap.addItem(potentialAnswer)
+                if answer is None:
+                    raise UsolvedRequirementException(
+                        "Expression requested another expression that is not solved yet"
+                    )
+                outputStack.push(answer)
+            elif expression[i] in "+-*/%^'":
+                unaryfixer = 1 if expression[i] == "'" else 0
+                while not operatorStack.isEmpty():
+                    op = operatorStack.pop()
+                    if not (precedence.get(op, 0) >= precedence[expression[i]] + unaryfixer):
+                        operatorStack.push(op)
+                        break
+                    if op == "'":
+                        operand = outputStack.pop()
+                        outputStack.push(
+                            Expression.calculateOperation(operand, None, op)
+                        )
+                    else:
+                        operand2 = outputStack.pop()
+                        operand1 = outputStack.pop()
+                        outputStack.push(
+                            Expression.calculateOperation(operand1, operand2, op)
+                        )
+                operatorStack.push(expression[i])
+                i += 1
+        while not operatorStack.isEmpty():
+            op = operatorStack.pop()
+            if op == "'":
+                operand = outputStack.pop()
+                outputStack.push(Expression.calculateOperation(operand, None, op))
             else:
-                operand2 = expStack[-1]
-                expStack.pop()
-                opration = expStack[-1]
-                expStack.pop()
-                operand1 = expStack[-1]
-                expStack.pop()
-                if opration == "'":
-                    expStack.append(operand1)
-                expStack.append(Expression.calculateOperation(operand1, operand2, opration))
-                operate = False
-        i += 1
-        return expStack[-1], i
+                operand2 = outputStack.pop()
+                operand1 = outputStack.pop()
+                outputStack.push(
+                    Expression.calculateOperation(operand1, operand2, op)
+                )
+        if i+2 < len(expression):
+            i += 1
+        return outputStack.pop(), i
+
     @staticmethod
     def calculateOperation(operand1, operand2, opration):
         if opration != "'":
-            if opration == '+':
+            if opration == "+":
                 return operand1 + operand2
-            elif opration == '-':
+            elif opration == "-":
                 return operand1 - operand2
-            elif opration == '*':
+            elif opration == "*":
                 return operand1 * operand2
-            elif opration == '/':
+            elif opration == "/":
                 return operand1 / operand2
-            elif opration == '%':
+            elif opration == "%":
                 return operand1 % operand2
-            elif opration == '^':
-                return operand1 ** operand2
+            elif opration == "^":
+                return operand1**operand2
         else:
             return 0 - operand1
+
+
 def main():
     heap = Heap()
     while True:
         expression = input("Enter an expression to solve or 's' to solve all:")
         if expression != "s":
-            expression = '(' + expression.replace(' ','').replace('\n','') + ')'
+            expression = "(" + expression.replace(" ", "").replace("\n", "") + ")"
             priority = input("Enter priority of expression:")
             heap.addItem(Expression(len(heap.array), int(priority), expression))
         else:
             break
     print(heap.solve())
+
+
 if __name__ == "__main__":
     main()
